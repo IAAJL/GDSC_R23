154be7f4b60473f58f5557fa6ab2136aabaa0cfa
IAAJL <iamaravindjl@gmail.com>
2023-10-15 15:44:39 +0530

    fourth commit

--- functions/functions.js
+++ functions/functions.js
@@ -1,13 +1,33 @@
 let conn = require('./connect')
-
+let collectname = 'users'
+let dbname = 'gdsc_r23'
 async function insert(){
     let client = await conn.getconnection();
-    let db = client.db('gdsc_r23')
+    let db = client.db(dbname)
     console.log("connected to db")
-    let collection = db.collection('users');
+    let collection = db.collection(collectname);
     console.log("Entered collection");
+    query={}
+    query.name ="hej"
+    await collection.insertOne(query).catch((error)=>{
+        console.log("Couldn't insert")
+    });
+    
+
     await client.close();
+    //res.send("Done");
+}
+async function deleteall(){
+    let client = await conn.getconnection();
+    let db = client.db(dbname)
+    console.log("connected to db")
+    let collection = db.collection(collectname);
+    console.log("Entered collection");
+
+    await collection.deleteMany()
+
+    client.close()
 }
 // conn.test();
-insert();
+deleteall()
 // module.export = {connectdb};
